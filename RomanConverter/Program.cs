4920d276c9f0f5557f82d2ffca79fe9e3fc624f1
Falco Böhnke <falco.boehnke@gmail.com>
2018-10-21 17:39:41 +0200

    Add files via upload

---
+++ RomanConverter/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Softwaredesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = int.Parse(args[0]);
+
+
+            Console.WriteLine(GetRomanNumber(value));
+
+        }
+
+        static string GetRomanNumber(int inp)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            if (inp >= 900)
+            {     //false
+                sb.Append("CM");
+                inp -= 900;
+            }
+            if (inp >= 500)
+            {     // true
+                sb.Append("D");        //"D"
+                inp -= 500;     // inp = 248
+            }
+            if (inp >= 400)
+            {     //false
+                sb.Append("CM");
+                inp -= 400;
+            }
+
+            if (inp >= 100)
+            {     // true
+                while (inp >= 100)
+                {
+                    sb.Append("C");    // "DCC"
+                    inp -= 100; // inp = 48
+                }
+            }
+
+            if (inp >= 90)
+            {  //false
+                sb.Append("XC");
+                inp -= 90;
+            }
+            if (inp >= 50)
+            {  // false
+                sb.Append("L");
+                inp -= 50;
+            }
+            if (inp >= 40)
+            {  // true
+                sb.Append("XL");   // DCCXL
+                inp -= 40;  // inp = 8
+            }
+
+            if (inp >= 10)
+            {  // false
+                while (inp >= 10)
+                {
+                    sb.Append("X");
+                    inp -= 10;
+                }
+            }
+
+            if (inp >= 9)
+            { // false
+                sb.Append("IX");
+                inp -= 9;
+            }
+            if (inp >= 5)
+            {   // true
+                sb.Append("V");    // "DCCXLV
+                inp -= 5;   // inp = 3
+            }
+            if (inp >= 4)
+            {   // false
+                sb.Append("IV");
+                inp -= 4;
+            }
+
+            if (inp >= 1)
+            {   // true
+                while (inp >= 1)
+                {
+                    sb.Append("I");    // "DCCXLVIII"
+                    inp -= 1; // inp = 0
+                }
+            }
+
+            return sb.ToString();
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
