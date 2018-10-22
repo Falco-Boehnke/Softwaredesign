547c8fa6204046903daabc145d4edafb39a0f59a
Shenanigator <falco.boehnke@gmail.com>
2018-10-22 21:50:40 +0200

    Can't get less lines

--- RomanConverter/Program.cs
+++ RomanConverter/Program.cs
@@ -1,70 +1,71 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace Softwaredesign
 {
     class Program
     {
         static int inp;
-        static string[,] romanDigits =
-        {
-                    {"CM", "D", "CD", "C"},
-                    {"XC", "L", "DL", "X"},
-                    {"IX", "V", "IV", "I"},
-        };
 
-        static int[,] divisionSteps =
-        {
-                    {900, 500, 400, 100},
-                    {90, 50, 40, 10},
-                    {9, 5, 4, 1},
+        static Dictionary<string, int> romanDecimalPairs = new Dictionary<string, int>() {
+            {"CM", 900},
+            {"D",  500},
+            {"CD", 400},
+            {"C",  100},
+            {"XC", 90},
+            {"L",  50},
+            {"DL", 40},
+            {"X",  10},
+            {"IX", 9},
+            {"V",  5},
+            {"IV", 4},
+            {"I",  1},
         };
+        static bool isInRange(int valueToCheck)
+        {
+            return (valueToCheck > 0 && valueToCheck < 1000);
+        }
 
         static void Main(string[] args)
         {
             try
             {
                 inp = int.Parse(args[0]);
-                if (inp < 1000 && inp > 0)
-                {
+                // cs0173 & cs0201 Error: isInRange(inp) ? Console.WriteLine(GetRomanNumber(inp)) : Console.WriteLine("Input muss aus einer Zahl zwischen 0 und 1000 bestehen");
+                if (isInRange(inp))
                     Console.WriteLine(GetRomanNumber(inp));
-                }
                 else
-                {
                     Console.WriteLine("Input muss aus einer Zahl zwischen 0 und 1000 bestehen");
-                }
             }
             catch (FormatException e)
             {
-                Console.Write(e.Message);
-                Console.WriteLine("Input darf nur aus zahlen bestehen");
+                Console.WriteLine(e.Message + ". Input darf nur aus zahlen bestehen");
             }                                    
         }
         
         static string GetRomanNumber(int inp)
         {
             StringBuilder sb = new StringBuilder();
-
-            for (int phase = 0; phase < 3; phase++)
+            foreach (var converterPair in romanDecimalPairs)
             {
-                for (int step = 0; step < 3; step++)
+                if (inp >= converterPair.Value)
                 {
-                    if (inp >= divisionSteps[phase, step])
-                    {     //false
-                        sb.Append(romanDigits[phase, step]);
-                        inp -= divisionSteps[phase, step];
-                    }
+                    sb.Append(converterPair.Key);
+                    inp -= converterPair.Value;
                 }
-
-                if (inp >= divisionSteps[phase, 3])
-                {     // true
-                    do
+                if (converterPair.Value == 100 || converterPair.Value == 10 || converterPair.Value == 1)
+                {   
+                    // inp is an int, therefore calculation-result will already be int and no cast/parse is needed
+                    int howManyTimes = inp / converterPair.Value;
+                    for (int i = 0; i < howManyTimes; i++)
                     {
-                        sb.Append(romanDigits[phase, 3]);    // "DCC"
-                        inp -= divisionSteps[phase, 3]; // inp = 48
-                    }
-                    while (inp >= divisionSteps[phase, 3]);
+                        sb.Append(converterPair.Key);
+                        inp -= converterPair.Value;
+                    }          
                 }
             }
             return sb.ToString();
