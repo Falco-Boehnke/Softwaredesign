09882bbfed93852fda1c8184d7d908619ff295bb
Falco Böhnke <falco.boehnke@gmail.com>
2018-12-09 01:21:39 +0100

    Add files via upload

---
+++ A8_Someoneelsesquiz/Quiz_v2/Quiz_v2/QuizBinary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_v2
+{
+    class QuizBinary : Quizelement
+    {
+
+        public override bool IsAnswerCorrect(int choice)
+        {
+            bool ffs = false;
+            if (choice == 1)
+                ffs = true;
+            if (choice == 2)
+                ffs = false;
+
+            if (ffs == answers[0].correct)
+                return true;
+            else
+                return false;
+        }
+        public override bool show()
+        {
+            Console.WriteLine(question + "Yes(1)/No(0)");
+            Console.WriteLine();
+            int choice = Int32.Parse(Console.ReadLine());
+
+            return IsAnswerCorrect(choice);
+        }
+
+        public override void createElement()
+        {
+            Console.WriteLine("Bitte Ja/Nein Frage eineben");
+            string text = Console.ReadLine();
+            this.question = text;
+
+            Console.WriteLine("Antwort korrekt (1) or nah (2)?");
+            int choice = Int32.Parse(Console.ReadLine());
+            bool correctOrNah = false;
+
+            if (choice == 1)
+                correctOrNah = true;
+
+
+            answers.Add(new Answer("", correctOrNah));
+        }
+    }
+}
---
+++ A8_Someoneelsesquiz/Quiz_v2/Quiz_v2/Quizelement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_v2
+{
+    public class Quizelement
+    {
+        public string question;
+        public List<Answer> answers;
+        public int correct;
+        public string callToAction;
+
+        public Quizelement()
+        {
+            answers = new List<Answer>();
+        }
+
+        public virtual bool IsAnswerCorrect(int correcter)
+        {
+            return false;
+        }
+
+        public virtual bool show()
+        {         
+            return false;
+        }
+
+        public virtual void createElement()
+        {
+
+        }
+    }
+}
