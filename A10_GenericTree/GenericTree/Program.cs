18e6d0e7b90b5cfddbc27a008705d385a2591272
Falco Böhnke <falco.boehnke@gmail.com>
2018-12-23 17:07:49 +0100

    Add files via upload

---
+++ A10_GenericTree/GenericTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<string> mainTree = new Tree<string>();
+            Node<string> root = mainTree.CreateNode("root");
+            Node<string> child1 = mainTree.CreateNode("child1");
+            Node<string> child2 = mainTree.CreateNode("child2");
+
+            Node<string> root2 = mainTree.CreateNode("root2");
+            Node<string> child11 = mainTree.CreateNode("child11");
+            Node<string> child12 = mainTree.CreateNode("child12");
+            Node<string> child13 = mainTree.CreateNode("child13");
+
+            Node<string> root3 = mainTree.CreateNode("root3");
+            Node<string> child21 = mainTree.CreateNode("child21");
+            Node<string> child22 = mainTree.CreateNode("child22");
+            Node<string> child23 = mainTree.CreateNode("child23");
+
+            Console.WriteLine("Reading Tree");
+            mainTree.OutputWholeTree();
+        }
+    }
+}
---
+++ A10_GenericTree/GenericTree/Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericTree
+{
+    class Tree<T>
+    {
+        List<Node<T>> allNodes;
+
+        public Tree()
+        {
+            allNodes = new List<Node<T>>();
+        }
+
+        public Node<T> CreateNode(T nodeValue)
+        {
+            Node<T> createdNode = new Node<T>(nodeValue);
+            createdNode.nodeValue = nodeValue;
+            allNodes.Add(createdNode);
+            return createdNode;
+        }
+
+        public void OutputWholeTree()
+        {
+            allNodes[0].OutputAllValues();
+        }
+
+    }
+}
