c703ec0d530afc88dd4fd38871f4ce9a703a423d
Falco Böhnke <falco.boehnke@gmail.com>
2018-12-23 17:06:34 +0100

    Add files via upload

---
+++ A9_GenericTree/GenericTree/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericTree
+{
+    class Node<T>
+    {
+        public T nodeValue;
+        public Node<T> nodeParent;
+        public List<Node<T>> nodeChildren;
+
+        private void init()
+        {
+            nodeChildren = new List<Node<T>>();
+        }
+        public Node(T nodeValue)
+        {
+            init();
+            this.nodeValue = nodeValue;        
+        }
+
+        public void AppendChildNode(Node<T> childNode)
+        {
+            nodeChildren.Add(childNode);
+        }
+
+
+        public void OutputAllValues()
+        {
+            Console.WriteLine(nodeValue);
+
+            if (nodeChildren.Count == 0)
+                return;
+
+            foreach (Node<T> node in nodeChildren)
+            {
+                node.OutputAllValues();
+            }
+        }
+    }
+}
